6a59e3555aa98ef2bb1fd2b3190ed89a4ee147ad
Martina Catizone <martina.catizone@student.manchester.ac.uk>
2017-10-29 12:23:46 +0000

    add lib to version control

---
+++ test/java/RentalcarsCodeTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalcarsCodeTest {
+    @Test
+    void parseJSON() {
+        List<Car> cars = new ArrayList<>();
+
+    }
+
+}
